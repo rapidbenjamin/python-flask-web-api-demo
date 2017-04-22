cbb17f347fc8676aaab093d4e91e0e74d436530d
rapidbenjamin <rapidbenjamin@gmail.com>
2017-04-23 01:13:15 +0200

    fixed issues

--- data/populate_sqlite.sql
+++ data/populate_sqlite.sql
@@ -1,46 +1,45 @@
-INSERT INTO `user`( id, email, username, password_hash, is_admin, is_owner, is_member, is_authenticated, is_anonymous, is_active, updated_at, created_at, locale, timezone ) VALUES ( 1, 'admin@example.com', 'admin@example.com', 'pbkdf2:sha1:1000$YOKWljpH$d2e8789a3ce4060778103f225314f14f0f985a7f', 1, 0, 1, 1, 0, 1, 1492357733, 1492293600, 'en_US', 'UTC' ); 
-INSERT INTO `user`( id, email, username, password_hash, is_admin, is_owner, is_member, is_authenticated, is_anonymous, is_active, updated_at, created_at, locale, timezone ) VALUES ( 2, 'editor@example.com', 'editor@example.com', 'pbkdf2:sha1:1000$mipuQL9v$3871eb3f3ee8ad8a7d322f100cf09acc6a997aa0', 1, 0, 1, 1, 0, 1, 1492419023, 1492419023, 'en_US', 'UTC' ); 
-
-INSERT INTO asset( id, data_file_name, data_content_type, data_file_size, asset_type, width, height, description_en_US, description_fr_FR, user_id, is_active, updated_at, created_at ) VALUES ( 1, 'avatar-systemaker-01.jpg', 'image/jpeg', 54370, 'image', 458, 458, 'description US', 'description FR', 1, 1, 1492471963, 1492293600 ); 
-INSERT INTO asset( id, data_file_name, data_content_type, data_file_size, asset_type, width, height, description_en_US, description_fr_FR, user_id, is_active, updated_at, created_at ) VALUES ( 3, 'avatar-imagineer-01.jpg', 'image/jpeg', 64188, 'image', 458, 458, '', '', null, 1, 1492472842, 1492466400 ); 
-INSERT INTO asset( id, data_file_name, data_content_type, data_file_size, asset_type, width, height, description_en_US, description_fr_FR, user_id, is_active, updated_at, created_at ) VALUES ( 4, 'test.txt', 'text/plain', 7, 'text', 0, 0, '', '', 1, 1, 1492627433, 1492625364 ); 
-
-INSERT INTO item( id, slug, title_en_US, title_fr_FR, description_en_US, description_fr_FR, amount, user_id, is_active, updated_at, created_at ) VALUES ( 1, 'product1', 'Product 1', 'Produit 1', '<span style="color: #ff0000;"><em><strong>description_en_US</strong></em></span>', 'description_fr_FR', 20.00, 1, 1, 1492643682, 1492293600 ); 
-INSERT INTO item( id, slug, title_en_US, title_fr_FR, description_en_US, description_fr_FR, amount, user_id, is_active, updated_at, created_at ) VALUES ( 2, 'product2', 'Product2', 'Produit 2', 'description_en_US', 'description_fr_FR', 0.00, 2, 1, 1492641888, 1492293600 ); 
-INSERT INTO item( id, slug, title_en_US, title_fr_FR, description_en_US, description_fr_FR, amount, user_id, is_active, updated_at, created_at ) VALUES ( 3, 'product3', 'product 3', 'produit 3', 'fdgdfgfdg', 'fdgdfgdfg', 10.00, 2, 1, 1492462741, 1492466400 ); 
-
-INSERT INTO `order`( id, status, user_id, amount, is_active, updated_at, created_at ) VALUES ( 1, 'paid', 2, 10.00, 1, 1492472842, 1492466400 ); 
-INSERT INTO `order`( id, status, user_id, amount, is_active, updated_at, created_at ) VALUES ( 2, 'pending', 1, 20.00, 1, 1492472842, 1492466400 ); 
-
-INSERT INTO orderitem( order_id, item_id, options, unit_amount, quantity, total_amount, updated_at, created_at ) VALUES ( 1, 2, null, 0.00, 1, 0.00, 1492472842, 1492472842 ); 
-INSERT INTO orderitem( order_id, item_id, options, unit_amount, quantity, total_amount, updated_at, created_at ) VALUES ( 1, 3, null, 10.00, 1, 10.00, 1492472842, 1492472842 ); 
-INSERT INTO orderitem( order_id, item_id, options, unit_amount, quantity, total_amount, updated_at, created_at ) VALUES ( 2, 1, null, 20.00, 1, 20.00, 1492472842, 1492472842 ); 
-
-INSERT INTO assetitem( asset_id, item_id, options, updated_at, created_at ) VALUES ( 1, 2, null, 1492357734, 1492357734 ); 
-INSERT INTO assetitem( asset_id, item_id, options, updated_at, created_at ) VALUES ( 1, 3, null, 1492462741, 1492462741 ); 
-INSERT INTO assetitem( asset_id, item_id, options, updated_at, created_at ) VALUES ( 4, 1, null, 1492625364, 1492625364 ); 
-
-INSERT INTO event( id, type, title_en_US, title_fr_FR, amount, user_id, item_id, start, end, days, allday, status, is_active, updated_at, created_at ) VALUES ( 1, 'meeting', 'meeting 1', 'meeting 1', 10.00, 1, 1, 1492552800, 1492639200, 1, 1, 'pending', 1, 1492565582, 1492564873 ); 
-INSERT INTO event( id, type, title_en_US, title_fr_FR, amount, user_id, item_id, start, end, days, allday, status, is_active, updated_at, created_at ) VALUES ( 2, 'booking', 'booking 1', 'booking 1', 100.00, 2, 3, 1492552800, 1492812000, 3, 1, 'pending', 1, 1492565582, 1492564873 ); 
-INSERT INTO event( id, type, title_en_US, title_fr_FR, amount, user_id, item_id, start, end, days, allday, status, is_active, updated_at, created_at ) VALUES ( 3, 'lesson', 'lesson 1', 'cours 1', 20.00, 1, 2, 1492552800, 1493416800, 10, 1, 'pending', 1, 1492565849, 1492565849 ); 
-
-INSERT INTO userevent( guest_id, in_event_id, options, updated_at, created_at ) VALUES ( 1, 1, null, 1492564873, 1492564873 ); 
-INSERT INTO userevent( guest_id, in_event_id, options, updated_at, created_at ) VALUES ( 1, 2, null, 1492564873, 1492564873 ); 
-INSERT INTO userevent( guest_id, in_event_id, options, updated_at, created_at ) VALUES ( 1, 3, null, 1492565849, 1492565849 ); 
-INSERT INTO userevent( guest_id, in_event_id, options, updated_at, created_at ) VALUES ( 2, 1, null, 1492564873, 1492564873 ); 
-INSERT INTO userevent( guest_id, in_event_id, options, updated_at, created_at ) VALUES ( 2, 3, null, 1492565849, 1492565849 ); 
-
-INSERT INTO section( id, slug, parent_id, title_en_US, title_fr_FR, description_en_US, description_fr_FR, is_active, updated_at, created_at ) VALUES ( 1, 'global', null, 'Global', 'Globale', 'description_en_US', 'description_fr_FR', 1, 1492471963, 1492466400 ); 
-INSERT INTO section( id, slug, parent_id, title_en_US, title_fr_FR, description_en_US, description_fr_FR, is_active, updated_at, created_at ) VALUES ( 2, 'section1', 1, 'Section 1', 'Section 1', 'description_en_US', 'description_fr_FR', 1, 1492471963, 1492466400 ); 
-INSERT INTO section( id, slug, parent_id, title_en_US, title_fr_FR, description_en_US, description_fr_FR, is_active, updated_at, created_at ) VALUES ( 3, 'section2', 1, 'Section 2', 'Section 2', 'description_en_US', 'description_fr_FR', 1, 1492471963, 1492466400 ); 
-
-INSERT INTO sectionitem( section_id, item_id, options, updated_at, created_at ) VALUES ( 1, 3, null, 1492636328, 1492636328 ); 
-INSERT INTO sectionitem( section_id, item_id, options, updated_at, created_at ) VALUES ( 2, 1, null, 1492636328, 1492636328 ); 
-INSERT INTO sectionitem( section_id, item_id, options, updated_at, created_at ) VALUES ( 3, 2, null, 1492636328, 1492636328 ); 
-INSERT INTO sectionitem( section_id, item_id, options, updated_at, created_at ) VALUES ( 3, 3, null, 1492636328, 1492636328 ); 
-
-INSERT INTO usersection( user_id, section_id, options, updated_at, created_at ) VALUES ( 1, 1, null, 1492636328, 1492636328 ); 
-INSERT INTO usersection( user_id, section_id, options, updated_at, created_at ) VALUES ( 1, 3, null, 1492636328, 1492636328 ); 
-INSERT INTO usersection( user_id, section_id, options, updated_at, created_at ) VALUES ( 2, 1, null, 1492636328, 1492636328 ); 
-INSERT INTO usersection( user_id, section_id, options, updated_at, created_at ) VALUES ( 2, 2, null, 1492636328, 1492636328 ); 
-
+BEGIN TRANSACTION;
+INSERT INTO `usersection` (user_id,section_id,options,updated_at,created_at) VALUES (1,1,NULL,1492636328,1492636328),
+ (1,3,NULL,1492636328,1492636328),
+ (2,1,NULL,1492636328,1492636328),
+ (2,2,NULL,1492636328,1492636328);
+INSERT INTO `userevent` (guest_id,in_event_id,options,updated_at,created_at) VALUES (1,1,NULL,1492564873,1492564873),
+ (1,2,NULL,1492564873,1492564873),
+ (1,3,NULL,1492565849,1492565849),
+ (2,1,NULL,1492564873,1492564873),
+ (2,3,NULL,1492565849,1492565849);
+INSERT INTO `useraddress` (guest_id,in_address_id,options,updated_at,created_at) VALUES (1,1,NULL,1492891938,1492891938),
+ (2,1,NULL,1492891938,1492891938),
+ (1,2,NULL,1492893366,1492893366),
+ (2,2,NULL,1492893366,1492893366);
+INSERT INTO `sectionitem` (section_id,item_id,options,updated_at,created_at) VALUES (1,3,NULL,1492636328,1492636328),
+ (2,1,NULL,1492636328,1492636328),
+ (3,2,NULL,1492636328,1492636328),
+ (3,3,NULL,1492636328,1492636328);
+INSERT INTO `orderitem` (order_id,item_id,options,unit_amount,quantity,total_amount,updated_at,created_at) VALUES (1,2,NULL,0,1,0,1492472842,1492472842),
+ (1,3,NULL,10,1,10,1492472842,1492472842),
+ (2,1,NULL,20,1,20,1492472842,1492472842),
+ (3,3,NULL,10,1,10,1492893945,1492893945);
+INSERT INTO `assetitem` (asset_id,item_id,options,updated_at,created_at) VALUES (1,2,NULL,1492357734,1492357734),
+ (1,3,NULL,1492462741,1492462741),
+ (4,1,NULL,1492625364,1492625364);
+INSERT INTO `User` (id,email,username,password_hash,is_admin,is_owner,is_member,is_authenticated,is_anonymous,is_active,updated_at,created_at,locale,timezone) VALUES (1,'admin@example.com','admin@example.com','pbkdf2:sha1:1000$YOKWljpH$d2e8789a3ce4060778103f225314f14f0f985a7f',1,0,1,1,0,1,1492357733,1492293600,'en_US','UTC'),
+ (2,'editor@example.com','editor@example.com','pbkdf2:sha1:1000$mipuQL9v$3871eb3f3ee8ad8a7d322f100cf09acc6a997aa0',1,0,1,1,0,1,1492419023,1492419023,'en_US','UTC');
+INSERT INTO `Section` (id,slug,parent_id,title_en_US,title_fr_FR,description_en_US,description_fr_FR,is_active,updated_at,created_at) VALUES (1,'global',NULL,'Global','Globale','description_en_US','description_fr_FR',1,1492471963,1492466400),
+ (2,'section1',1,'Section 1','Section 1','description_en_US','description_fr_FR',1,1492471963,1492466400),
+ (3,'section2',1,'Section 2','Section 2','description_en_US','description_fr_FR',1,1492471963,1492466400);
+INSERT INTO `Order` (id,status,user_id,amount,is_active,updated_at,created_at) VALUES (1,'paid',2,10,1,1492472842,1492466400),
+ (2,'pending',1,20,1,1492472842,1492466400),
+ (3,'cart',1,10,1,1492893945,1492893945);
+INSERT INTO `Item` (id,slug,type,title_en_US,title_fr_FR,description_en_US,description_fr_FR,amount,user_id,is_active,updated_at,created_at) VALUES (1,'product1','book','Product 1','Produit 1','<span style="color: #ff0000;"><em><strong>description_en_US</strong></em></span>','description_fr_FR',20,1,1,1492893945,1492293600),
+ (2,'product2','cloth','Product2','Produit 2','description_en_US','description_fr_FR',0,2,1,1492893945,1492293600),
+ (3,'product3','tool','product 3','produit 3','fdgdfgfdg','fdgdfgdfg',10,2,1,1492893945,1492466400);
+INSERT INTO `Event` (id,type,title_en_US,title_fr_FR,description_en_US,description_fr_FR,amount,user_id,item_id,address_id,start,end,days,allday,status,is_active,updated_at,created_at) VALUES (1,'meeting','meeting 1','meeting 1','','',10,1,1,1,1492552800,1492639200,1,1,'pending',1,1492891938,1492564873),
+ (2,'booking','booking 1','booking 1','','',100,2,3,1,1492552800,1492812000,3,1,'pending',1,1492891938,1492564873),
+ (3,'lesson','lesson 1','cours 1','','',20,1,1,2,1492552800,1493416800,10,1,'pending',1,1492893946,1492565849);
+INSERT INTO `Asset` (id,data_file_name,data_content_type,data_file_size,asset_type,width,height,description_en_US,description_fr_FR,user_id,is_active,updated_at,created_at) VALUES (1,'avatar-systemaker-01.jpg','image/jpeg',54370,'image',458,458,'description US','description FR',1,1,1492471963,1492293600),
+ (3,'avatar-imagineer-01.jpg','image/jpeg',64188,'image',458,458,'','',NULL,1,1492472842,1492466400),
+ (4,'test.txt','text/plain',7,'text',0,0,'','',1,1,1492627433,1492625364);
+INSERT INTO `Address` (id,type,title_en_US,title_fr_FR,address_line1,address_line2,city,postal_code,state_region,country,full,time_zone,latitude,longitude,amount,user_id,item_id,status,is_active,updated_at,created_at) VALUES (1,'office','my office','mon bureau','5 Avenue Anatole France','','Paris','75007','Ile de france','France','5 Avenue Anatole France, Paris, 75007, Ile de france, France','2',48.86,2.29,20,1,1,'start',1,1492893366,1492891316),
+ (2,'home','my home','ma maison','Champs-Élysées','','Paris','75008','Ile de france','France','Champs-Élysées, Paris, 75008, Ile de france, France','2',48.87,2.31,30,2,1,'start',1,1492893946,1492893366);
+COMMIT;
--- data/schema_sqlite.sql
+++ data/schema_sqlite.sql
@@ -1,29 +1,246 @@
-
-INSERT INTO `User` (id,email,username,password_hash,is_admin,is_owner,is_member,is_authenticated,is_anonymous,is_active,updated_at,created_at,locale,timezone) VALUES (1,'admin@example.com','admin@example.com','pbkdf2:sha1:1000$YOKWljpH$d2e8789a3ce4060778103f225314f14f0f985a7f',1,0,1,1,0,1,1492357733,1492293600,'en_US','UTC'),
- (2,'editor@example.com','editor@example.com','pbkdf2:sha1:1000$mipuQL9v$3871eb3f3ee8ad8a7d322f100cf09acc6a997aa0',1,0,1,1,0,1,1492419023,1492419023,'en_US','UTC');
-
-INSERT INTO `Section` (id,slug,parent_id,title_en_US,title_fr_FR,description_en_US,description_fr_FR,is_active,updated_at,created_at) VALUES (1,'section1',3,'Section 1','Section 1','description_en_US','description_fr_FR',1,1492471963,1492466400),
- (2,'section2',3,'Section 2','Section 2','description_en_US','description_fr_FR',1,1492471963,1492466400),
- (3,'global',NULL,'Global','Globale','description_en_US','description_fr_FR',1,1492471963,1492466400);
-
-INSERT INTO `Order` (id,status,user_id,amount,is_active,updated_at,created_at) VALUES (1,'paid',2,10,1,1492472842,1492466400),
- (2,'pending',1,20,1,1492472842,1492466400);
-
-INSERT INTO `Item` (id,slug,title_en_US,title_fr_FR,description_en_US,description_fr_FR,amount,user_id,is_active,updated_at,created_at) VALUES (1,'product1','Product 1','Produit 1','description_en_US','description_fr_FR',20,1,1,1492456877,1492293600),
- (2,'product2','Product2','Produit 2','description_en_US','description_fr_FR',0,2,1,1492459454,1492293600),
- (3,'product3','product 3','produit 3','fdgdfgfdg','fdgdfgdfg',10,2,1,1492462741,1492466400);
-
-INSERT INTO `Asset` (id,data_file_name,data_content_type,data_file_size,asset_type,width,height,description_en_US,description_fr_FR,user_id,is_active,updated_at,created_at) VALUES (1,'avatar-systemaker-01.jpg','image/jpeg',54370,'image',458,458,'description US','description FR',1,1,1492471963,1492293600),
- (3,'avatar-imagineer-01.jpg','image/jpeg',64188,'image',458,458,'','',NULL,1,1492472842,1492466400);
-
- 
-INSERT INTO `sectionitem` (section_id,item_id,options,updated_at,created_at) VALUES (1,1,NULL,1492466221,1492466221),
- (2,2,NULL,1492466221,1492466221),
- (3,1,NULL,1492471963,1492471963);
-
-INSERT INTO `orderitem` (order_id,item_id,options,unit_amount,quantity,total_amount,updated_at,created_at) VALUES (1,2,NULL,0,1,0,1492472842,1492472842),
- (1,3,NULL,10,1,10,1492472842,1492472842),
- (2,1,NULL,20,1,20,1492472842,1492472842);
-
-INSERT INTO `assetitem` (asset_id,item_id,options,updated_at,created_at) VALUES (1,2,NULL,1492357734,1492357734),
- (1,3,NULL,1492462741,1492462741);
+BEGIN TRANSACTION;
+CREATE TABLE usersection (
+	user_id INTEGER NOT NULL, 
+	section_id INTEGER NOT NULL, 
+	options TEXT, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (user_id, section_id), 
+	FOREIGN KEY(user_id) REFERENCES "User" (id), 
+	FOREIGN KEY(section_id) REFERENCES "Section" (id)
+);
+CREATE TABLE userevent (
+	guest_id INTEGER NOT NULL, 
+	in_event_id INTEGER NOT NULL, 
+	options TEXT, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (guest_id, in_event_id), 
+	FOREIGN KEY(guest_id) REFERENCES "User" (id), 
+	FOREIGN KEY(in_event_id) REFERENCES "Event" (id)
+);
+CREATE TABLE useraddress (
+	guest_id INTEGER NOT NULL, 
+	in_address_id INTEGER NOT NULL, 
+	options TEXT, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (guest_id, in_address_id), 
+	FOREIGN KEY(guest_id) REFERENCES "User" (id), 
+	FOREIGN KEY(in_address_id) REFERENCES "Address" (id)
+);
+CREATE TABLE sectionitem (
+	section_id INTEGER NOT NULL, 
+	item_id INTEGER NOT NULL, 
+	options TEXT, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (section_id, item_id), 
+	FOREIGN KEY(section_id) REFERENCES "Section" (id), 
+	FOREIGN KEY(item_id) REFERENCES "Item" (id)
+);
+CREATE TABLE orderitem (
+	order_id INTEGER NOT NULL, 
+	item_id INTEGER NOT NULL, 
+	options TEXT, 
+	unit_amount NUMERIC(10, 2) NOT NULL, 
+	quantity INTEGER NOT NULL, 
+	total_amount NUMERIC(10, 2) NOT NULL, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (order_id, item_id), 
+	FOREIGN KEY(order_id) REFERENCES "Order" (id), 
+	FOREIGN KEY(item_id) REFERENCES "Item" (id)
+);
+CREATE TABLE assetitem (
+	asset_id INTEGER NOT NULL, 
+	item_id INTEGER NOT NULL, 
+	options TEXT, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (asset_id, item_id), 
+	FOREIGN KEY(asset_id) REFERENCES "Asset" (id), 
+	FOREIGN KEY(item_id) REFERENCES "Item" (id)
+);
+CREATE TABLE "User" (
+	id INTEGER NOT NULL, 
+	email VARCHAR(60), 
+	username VARCHAR(60), 
+	password_hash VARCHAR(128), 
+	is_admin BOOLEAN, 
+	is_owner BOOLEAN, 
+	is_member BOOLEAN, 
+	is_authenticated BOOLEAN, 
+	is_anonymous BOOLEAN, 
+	is_active BOOLEAN, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	locale VARCHAR(30), 
+	timezone VARCHAR(60), 
+	PRIMARY KEY (id), 
+	CHECK (is_admin IN (0, 1)), 
+	CHECK (is_owner IN (0, 1)), 
+	CHECK (is_member IN (0, 1)), 
+	CHECK (is_authenticated IN (0, 1)), 
+	CHECK (is_anonymous IN (0, 1)), 
+	CHECK (is_active IN (0, 1))
+);
+CREATE TABLE "Section" (
+	id INTEGER NOT NULL, 
+	slug VARCHAR(255), 
+	parent_id INTEGER, 
+	"title_en_US" VARCHAR(255), 
+	"title_fr_FR" VARCHAR(255), 
+	"description_en_US" TEXT, 
+	"description_fr_FR" TEXT, 
+	is_active BOOLEAN, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(parent_id) REFERENCES "Section" (id), 
+	CHECK (is_active IN (0, 1))
+);
+CREATE TABLE "Order" (
+	id INTEGER NOT NULL, 
+	status VARCHAR(30), 
+	user_id INTEGER, 
+	amount NUMERIC(10, 2) NOT NULL, 
+	is_active BOOLEAN, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(user_id) REFERENCES "User" (id), 
+	CHECK (is_active IN (0, 1))
+);
+CREATE TABLE "Item" (
+	id INTEGER NOT NULL, 
+	slug VARCHAR(255), 
+	type VARCHAR(255), 
+	"title_en_US" VARCHAR(255), 
+	"title_fr_FR" VARCHAR(255), 
+	"description_en_US" TEXT, 
+	"description_fr_FR" TEXT, 
+	amount NUMERIC(10, 2) NOT NULL, 
+	user_id INTEGER, 
+	is_active BOOLEAN NOT NULL, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(user_id) REFERENCES "User" (id), 
+	CHECK (is_active IN (0, 1))
+);
+CREATE TABLE "Event" (
+	id INTEGER NOT NULL, 
+	type VARCHAR(255), 
+	"title_en_US" VARCHAR(255), 
+	"title_fr_FR" VARCHAR(255), 
+	"description_en_US" TEXT, 
+	"description_fr_FR" TEXT, 
+	amount NUMERIC(10, 2) NOT NULL, 
+	user_id INTEGER, 
+	item_id INTEGER, 
+	address_id INTEGER, 
+	start INTEGER, 
+	"end" INTEGER, 
+	days INTEGER, 
+	allday BOOLEAN, 
+	status VARCHAR(255), 
+	is_active BOOLEAN NOT NULL, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(user_id) REFERENCES "User" (id), 
+	FOREIGN KEY(item_id) REFERENCES "Item" (id), 
+	FOREIGN KEY(address_id) REFERENCES "Address" (id), 
+	CHECK (allday IN (0, 1)), 
+	CHECK (is_active IN (0, 1))
+);
+CREATE TABLE "Asset" (
+	id INTEGER NOT NULL, 
+	data_file_name VARCHAR(255), 
+	data_content_type VARCHAR(255), 
+	data_file_size INTEGER, 
+	asset_type VARCHAR(30), 
+	width INTEGER, 
+	height INTEGER, 
+	"description_en_US" TEXT, 
+	"description_fr_FR" TEXT, 
+	user_id INTEGER, 
+	is_active BOOLEAN, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(user_id) REFERENCES "User" (id), 
+	CHECK (is_active IN (0, 1))
+);
+CREATE TABLE "Address" (
+	id INTEGER NOT NULL, 
+	type VARCHAR(255), 
+	"title_en_US" VARCHAR(255), 
+	"title_fr_FR" VARCHAR(255), 
+	address_line1 VARCHAR(255), 
+	address_line2 VARCHAR(255), 
+	city VARCHAR(255), 
+	postal_code VARCHAR(255), 
+	state_region VARCHAR(255), 
+	country VARCHAR(255), 
+	"full" VARCHAR(255), 
+	time_zone VARCHAR(255), 
+	latitude NUMERIC(9, 6) NOT NULL, 
+	longitude NUMERIC(9, 6) NOT NULL, 
+	amount NUMERIC(10, 2) NOT NULL, 
+	user_id INTEGER, 
+	item_id INTEGER, 
+	status VARCHAR(255), 
+	is_active BOOLEAN NOT NULL, 
+	updated_at INTEGER, 
+	created_at INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY(user_id) REFERENCES "User" (id), 
+	FOREIGN KEY(item_id) REFERENCES "Item" (id), 
+	CHECK (is_active IN (0, 1))
+);
+CREATE UNIQUE INDEX "ix_User_username" ON "User" (username);
+CREATE INDEX "ix_User_timezone" ON "User" (timezone);
+CREATE INDEX "ix_User_locale" ON "User" (locale);
+CREATE INDEX "ix_User_is_active" ON "User" (is_active);
+CREATE UNIQUE INDEX "ix_User_email" ON "User" (email);
+CREATE UNIQUE INDEX "ix_Section_title_fr_FR" ON "Section" ("title_fr_FR");
+CREATE UNIQUE INDEX "ix_Section_title_en_US" ON "Section" ("title_en_US");
+CREATE UNIQUE INDEX "ix_Section_slug" ON "Section" (slug);
+CREATE INDEX "ix_Section_parent_id" ON "Section" (parent_id);
+CREATE INDEX "ix_Section_is_active" ON "Section" (is_active);
+CREATE INDEX "ix_Order_status" ON "Order" (status);
+CREATE INDEX "ix_Order_is_active" ON "Order" (is_active);
+CREATE INDEX "ix_Item_type" ON "Item" (type);
+CREATE UNIQUE INDEX "ix_Item_title_fr_FR" ON "Item" ("title_fr_FR");
+CREATE UNIQUE INDEX "ix_Item_title_en_US" ON "Item" ("title_en_US");
+CREATE UNIQUE INDEX "ix_Item_slug" ON "Item" (slug);
+CREATE INDEX "ix_Item_is_active" ON "Item" (is_active);
+CREATE INDEX "ix_Event_type" ON "Event" (type);
+CREATE UNIQUE INDEX "ix_Event_title_fr_FR" ON "Event" ("title_fr_FR");
+CREATE UNIQUE INDEX "ix_Event_title_en_US" ON "Event" ("title_en_US");
+CREATE INDEX "ix_Event_status" ON "Event" (status);
+CREATE INDEX "ix_Event_start" ON "Event" (start);
+CREATE INDEX "ix_Event_is_active" ON "Event" (is_active);
+CREATE INDEX "ix_Event_end" ON "Event" ("end");
+CREATE INDEX "ix_Event_days" ON "Event" (days);
+CREATE INDEX "ix_Event_allday" ON "Event" (allday);
+CREATE INDEX "ix_Asset_is_active" ON "Asset" (is_active);
+CREATE INDEX "ix_Asset_asset_type" ON "Asset" (asset_type);
+CREATE INDEX "ix_Address_type" ON "Address" (type);
+CREATE UNIQUE INDEX "ix_Address_title_fr_FR" ON "Address" ("title_fr_FR");
+CREATE UNIQUE INDEX "ix_Address_title_en_US" ON "Address" ("title_en_US");
+CREATE INDEX "ix_Address_time_zone" ON "Address" (time_zone);
+CREATE INDEX "ix_Address_status" ON "Address" (status);
+CREATE INDEX "ix_Address_state_region" ON "Address" (state_region);
+CREATE INDEX "ix_Address_postal_code" ON "Address" (postal_code);
+CREATE INDEX "ix_Address_longitude" ON "Address" (longitude);
+CREATE INDEX "ix_Address_latitude" ON "Address" (latitude);
+CREATE INDEX "ix_Address_is_active" ON "Address" (is_active);
+CREATE INDEX "ix_Address_full" ON "Address" ("full");
+CREATE INDEX "ix_Address_country" ON "Address" (country);
+CREATE INDEX "ix_Address_city" ON "Address" (city);
+CREATE INDEX "ix_Address_address_line2" ON "Address" (address_line2);
+CREATE INDEX "ix_Address_address_line1" ON "Address" (address_line1);
+COMMIT;
